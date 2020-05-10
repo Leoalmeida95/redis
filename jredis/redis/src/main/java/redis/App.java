44fd1d7fad4203fa5dcdb56259bcdb58eb30bd64
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-10 12:33:07 -0300

    maven configuration

--- jredis/redis/src/main/java/redis/App.java
+++ jredis/redis/src/main/java/redis/App.java
@@ -1,12 +1,10 @@
-package j_redis.redis.src.main.java.redis;
+package redis;
 
 import java.util.Scanner;
+import redis.clients.jedis.Jedis;
 
 public class App 
 {
-
-    static Scanner teclado = new Scanner(System.in);
-
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
