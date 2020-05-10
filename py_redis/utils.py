d05f107eec60afeffc3ea52332388a87d2cff0c4
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-09 22:10:49 -0300

    inserir e incrementar id

--- py_redis/utils.py
+++ py_redis/utils.py
@@ -1,5 +1,19 @@
 import redis
 
+def gera_id():
+    try:
+        conn = conectar()
+        chave = conn.get('chave')
+
+        if chave:
+            chave = conn.incr('chave')
+            return chave
+        else:
+            conn.set('chave',1)
+            return 1
+    except redis.exceptions.ConnectionError as e:
+        print(f'Não foi possível gerar a chave: {e}')
+
 def conectar():
     conn = redis.Redis(host='localhost', port=6379)
 
@@ -29,4 +43,26 @@ def listar():
             print('Não foram encontrados produtos')
     except redis.exceptions.ConnectionError as e:
         print(f'Não foi possível listar os produtos. {e}')
+
+    desconectar(conn)
+
+def inserir():
+    conn = conectar()
+
+    nome = input('Informe o nome do produto:')
+    preco = input('Informe o preco do produto:')
+    estoque = input('Informe o estoque:')
+
+    produto = {"nome": nome, "preco": preco, "estoque": estoque}
+    chave = f'produtos:{gera_id()}'
+
+    try:
+        res = conn.hmset(chave, produto)
+        if res:
+            print('O produto {nome} foi inserido com sucesso!')
+        else:
+            print('Não foi possível inserir o produto.')
+    except redis.exceptions.ConnectionError as e:
+        print(f'Não foi possível inseror o produto. {e}')
+    
     desconectar(conn)
