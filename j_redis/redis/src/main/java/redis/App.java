270bc327ba2fd5b3eb4fbee2ab4365643ec4b3bc
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-10 12:16:53 -0300

    projeto base java

---
+++ j_redis/redis/src/main/java/redis/App.java
@@ -0,0 +1,14 @@
+package j_redis.redis.src.main.java.redis;
+
+import java.util.Scanner;
+
+public class App 
+{
+
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
