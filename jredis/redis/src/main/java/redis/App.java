a10905db397d005d841bb47b71f9a236c2a758b2
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-05-10 12:35:29 -0300

    connect to redis in java

--- jredis/redis/src/main/java/redis/App.java
+++ jredis/redis/src/main/java/redis/App.java
@@ -5,8 +5,15 @@ import redis.clients.jedis.Jedis;
 
 public class App 
 {
+    public static Jedis conectar(){
+        Jedis conn = new Jedis("localhost");
+
+        return conn;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        conectar();
+        System.out.println( "Redis is running on java application..." );
     }
 }
